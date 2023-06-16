01a842958e2f8983ce82c6b193a0f0771e5aebbd
MiggyMadet <miggymadet22@gmail.com>
2023-06-16 09:16:27 +0800

    gallery activity

---
+++ src/components/explore/components/modal-components/modal-comment-section.vue
@@ -0,0 +1,63 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+    <div style="padding-left: 20px;">Comments</div>
+            <div class="comment-section">
+              <div class="comment">
+                <div class="comment-layout">
+                  <img src="src/components/explore/components/assets/cat-pic.jpg" class="commenter-image" />
+                </div>
+                <div class="comment-layout">
+                  This is first comment.
+                </div>
+              </div>
+              <div class="comment">
+                  This is second comment.
+              </div>
+              <div class="comment">
+                  This is third comment.
+              </div>
+              <div class="comment">
+                  This is fourth comment.
+              </div>
+              <div class="comment">
+                  This is fifth comment.
+              </div>
+              <div class="comment">
+                  This is sixth comment.
+              </div>
+          </div>
+</template>
+
+<style lang="scss">
+/* Comment Section */
+.comment-section {
+    max-height: 25vh;
+    max-width: 100%;
+    overflow-y: scroll;
+}
+ 
+.comment {
+    height: 10%;
+    margin: 2%;
+    padding: 20px;
+    background-color: #ffff;
+    color: black;
+}
+
+.comment-layout {
+    width: 50%;
+    padding: 20px;
+    float: left;
+}
+
+.commenter-image {
+    width: 10%;
+    height: auto;
+    border-radius: 50%;
+    position: absolute;
+    top: 30px;
+    left: 30px;
+}
+</style>
--- src/components/explore/components/profile-banner.vue
+++ src/components/explore/components/profile-banner.vue
@@ -6,44 +6,67 @@ const props = defineProps<{
 </script>
 
 <template>
-    <div class="container drop-shadow">
-        <div>
+    <div class="profile-card">
+        <div class="profile-image">
         <slot name="user-img">
-            <img :src=src class="rounded-corners" />
+            <img :src=src class="profile-image__img" />
         </slot>
         </div>
-        <div class="item details">
-            <slot name="user-name"></slot>
+        <div class="profile-info">
+            <span>Profile of:</span>
+            <h3><slot name="user-name"></slot></h3>
+            <p>User Bio</p>
         </div>
     </div>
 </template>
 
 <style lang="scss">
-img.rounded-corners {
-  border-radius: 50%;
-  width: 100px;
-  height: 100px;
+.profile-card {
+    width: 100%;
+    max-width: 700px;
+    margin: 0 auto;
+    padding: 30px;
+    box-sizing: border-box;
+    border-radius: 20px;
+    background-color: #fff;
+    overflow: hidden;
+    position: relative;
+    min-height: 150px;
+    margin-bottom: 2rem;
 }
 
-.container {
-    display: flex;
-    border-radius: 5px;
-    width: 30rem;
+.profile-card .profile-image__img {
+    width: 10%;
+    height: auto;
+    border-radius: 50%;
+    position: absolute;
+    top: 30px;
+    left: 30px;
 }
 
-.item {
-    flex: 1;
+
+.profile-info {
+    width: 85%;
+    float: right;
+    padding-left: 10px;
+    box-sizing: border-box;
 }
 
-.details {
-    display: flex;
-    justify-content: center;
-    align-items: center;
+.profile-info span {
+    text-transform: uppercase;
+    color: #666;
+    letter-spacing: 3px;
 }
 
-.drop-shadow {
-    border: 1px solid;
-    padding: 10px;
-    box-shadow: 4px 3px;
+.profile-info h3 {
+    margin: 10px 0;
+    font-weight: 700;
+    font-size: 1.5em;
+    color: #222;
+}
+
+.profile-info p {
+    line-height: 140%;
+    color: #666;
 }
 </style>
